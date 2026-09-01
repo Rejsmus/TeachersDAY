8fea02c0815a30bdedada90c1e61192c567979ac
Rejsmus <150793323+Rejsmus@users.noreply.github.com>
2026-09-01 21:31:54 +1000

    Delete TeacherDay directory

--- TeacherDay/app/src/main/java/com/example/teacherday/Lesson.kt
+++
@@ -1,7 +0,0 @@
-package com.example.teacherday
-
-data class Lesson(
-    val className: String,
-    val lessonNumber: Int,
-    val subject: String
-)
--- TeacherDay/app/src/main/java/com/example/teacherday/LessonItem.kt
+++
@@ -1,8 +0,0 @@
-package com.example.teacherday
-
-data class LessonItem(
-    val day: String,
-    val lessonNumber: String,
-    val timeRange: String,
-    val duration: String
-)
